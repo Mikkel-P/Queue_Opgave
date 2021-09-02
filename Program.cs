893458bbb53c33b7a807941efc03edce7fd7783c
Mikkel Pedersen <mikk79h6@zbc.dk>
2021-09-02 10:43:36 +0200

    Tilføjede en Dequeue funktion til vores PrintAll metode.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Queue_Opgave
     class Program
     {
         // Vi lægger vores queue her så vi kan kalde på den i alle metoder
-        static Queue<PrinterJob> newQueue = new Queue<PrinterJob>();
+        static Queue<PrinterJob> printQueue = new Queue<PrinterJob>();
         static void Main(string[] args)
         {
             // Her genererer vi en random der dikterer størrelsen på vores filer
@@ -20,22 +20,22 @@ namespace Queue_Opgave
             job1.FileName = "Kittens";
             job1.FileType = "jpg";
             job1.FileSize = rand.Next(1000, 50000);
-            newQueue.Enqueue(job1);
+            printQueue.Enqueue(job1);
             PrinterJob job2 = new PrinterJob();
             job2.FileName = "Movies";
             job2.FileType = "txt";
             job2.FileSize = rand.Next(1000, 50000);
-            newQueue.Enqueue(job2);
+            printQueue.Enqueue(job2);
             PrinterJob job3 = new PrinterJob();
             job3.FileName = "bomb manual";
             job3.FileType = "txt";
             job3.FileSize = rand.Next(1000, 50000);
-            newQueue.Enqueue(job3);
+            printQueue.Enqueue(job3);
             PrinterJob job4 = new PrinterJob();
             job4.FileName = "Switch Configuration";
             job4.FileType = "txt";
             job4.FileSize = rand.Next(1000, 50000);
-            newQueue.Enqueue(job4);
+            printQueue.Enqueue(job4);
 
             // En boolean til at vise vores menu
             bool showMenu = true;
@@ -93,7 +93,7 @@ namespace Queue_Opgave
             PrinterJob newFile = new PrinterJob();
 
             // Tilføjer et nyt objekt til vores queue
-            newQueue.Enqueue(newFile);
+            printQueue.Enqueue(newFile);
 
             // Her genererer vi en random der dikterer størrelsen på vores filer
             Random random = new Random();
@@ -122,7 +122,7 @@ namespace Queue_Opgave
             // stemmer overens med et der allerede eksisterer, og sletter det efterfølgende. Vi benytter LinQ
             // til at udelade en specifik plads på vores nye liste med .Where da en queue normalt kører efter
             // principperne first in - first out.
-            newQueue = new Queue<PrinterJob>(newQueue.Where(job => (job.FileName != fileInput && job.FileType != typeInput)));
+            printQueue = new Queue<PrinterJob>(printQueue.Where(job => (job.FileName != fileInput && job.FileType != typeInput)));
 
             Console.WriteLine("Your file has been deleted, succesfully.");
         }
@@ -130,19 +130,19 @@ namespace Queue_Opgave
         {
             Console.Clear();
             // Vi bruger  .Count for at tælle antal elementer i vores queue
-            Console.WriteLine(newQueue.Count);
+            Console.WriteLine(printQueue.Count);
             Console.ReadLine();
-            foreach (PrinterJob item in newQueue)
+            foreach (PrinterJob item in printQueue)
             {
-                    Console.WriteLine(item.FileName + "found.");
+                Console.WriteLine(item.FileName + "found.");
             }
         }
         static void ShowMinMax()
         {
             // Vi bruger LinQ's lambda expression for at pege på hvor der skal ledes efter en minimums/maksimums værdi
             Console.Clear();
-            Console.WriteLine($"The Smallest file in the queue {newQueue.Min(job => job.FileSize)}");
-            Console.WriteLine($"The Biggest file in the queue {newQueue.Max(job => job.FileSize)}");
+            Console.WriteLine($"The Smallest file in the queue {printQueue.Min(job => job.FileSize)}");
+            Console.WriteLine($"The Biggest file in the queue {printQueue.Max(job => job.FileSize)}");
             Console.ReadLine();
         }
         static void FindItem()
@@ -154,7 +154,7 @@ namespace Queue_Opgave
             string filename = Console.ReadLine();
 
             // Udskriver det objekt som brugeren leder efter til skærmen, hvis det eksisterer i 
-            foreach (PrinterJob item in newQueue)
+            foreach (PrinterJob item in printQueue)
             {
                 if (filename == item.FileName)
                 {
@@ -166,8 +166,10 @@ namespace Queue_Opgave
         {
             Console.Clear();
 
-            foreach (PrinterJob item in newQueue)
+            while (printQueue.Count > 0)
             {
+                PrinterJob item = printQueue.Dequeue();
+
                 // Her udskrives alle detaljer om objektet i printerkøen
                 Console.WriteLine(item.FileName + "." + item.FileType + " " + item.FileSize + "kb - Is now being printed.");
 
@@ -178,19 +180,34 @@ namespace Queue_Opgave
                     System.Threading.Thread.Sleep(200);
                     Console.Write(".");
                 }
-                Console.WriteLine();
+                Console.WriteLine(); 
             }
+
+            //foreach (PrinterJob item in printQueue)
+            //{
+            //    // Her udskrives alle detaljer om objektet i printerkøen
+            //    Console.WriteLine(item.FileName + "." + item.FileType + " " + item.FileSize + "kb - Is now being printed.");
+
+            //    for (int i = 0; i < 20; i++)
+            //    {
+            //        // System.Threading.Thread.Sleep benyttes til at sætte en timer imellem
+            //        // vores punktummer så vi kan simulere en loading proces
+            //        System.Threading.Thread.Sleep(200);
+            //        Console.Write(".");
+            //    }
+            //    Console.WriteLine();
+            //}
             Console.WriteLine("All items succesfully printed.");
             Console.ReadLine();
         }
         static void ExitMenu()
         {
-        // Her opfanger vi brugerens ønske om at lukke applikationen
-        string exit = Console.ReadLine();
-        // Med Environment.Exit lukker vi det miljø vores applikation kører i
-        // Vi er nød til at lave en Convert.ToInt32 da Environment.Exit kræver
-        // en int data type for at kunne udføres
-        Environment.Exit(Convert.ToInt32(exit));
+            // Her opfanger vi brugerens ønske om at lukke applikationen
+            string exit = Console.ReadLine();
+            // Med Environment.Exit lukker vi det miljø vores applikation kører i
+            // Vi er nød til at lave en Convert.ToInt32 da Environment.Exit kræver
+            // en int data type for at kunne udføres
+            Environment.Exit(Convert.ToInt32(exit));
         }
     }
 }
